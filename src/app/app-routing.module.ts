ba966f31f5a29efabea8869c3fe84f719f70ba89
RenatoDiPierno <renatodipi@gmail.com>
2019-04-03 12:15:58 +0200

    routing OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RoutingEnum } from './routingEnum';
+import { HomeComponent } from './components/home/home.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { ProfiloComponent } from './components/profilo/profilo.component';
 
-const routes: Routes = [];
+export const appRoutes: Routes = [
+  {
+    path: RoutingEnum.base, children: [
+      { path: RoutingEnum.home, component: HomeComponent },
+      { path: RoutingEnum.product, component: ProductListComponent },
+      { path: RoutingEnum.cards, component: CardsComponent },
+      { path: RoutingEnum.feedback, component: FeedbackComponent },
+      { path: RoutingEnum.profilo, component: ProfiloComponent },
+      
+      { path: '', redirectTo: RoutingEnum.base + '/' + RoutingEnum.home, pathMatch: 'full' },
+    ],
+    //canActivateChild: [LoginControlsService]
+  },
+  //{path: RoutingEnum.login, component: LoginComponent},
+  //{ path: '', redirectTo: '/' + RoutingEnum.login, pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/routingEnum.ts
@@ -0,0 +1,9 @@
+export enum RoutingEnum{
+    base = 'ShoesSelling',
+    home = 'home',
+    product = 'product-list',
+    cards = 'cards',
+    feedback = 'feedback',
+    profilo = 'profilo',
+    login = 'login',
+}
