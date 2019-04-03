e3636b15e208dc2185491887f3259ab1c01881fd
lscutillo <lscutillo@gmail.com>
2019-04-03 10:43:30 +0200

    creazione component tranne login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { ProfiloComponent } from './components/profilo/profilo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductListComponent,
+    MenuComponent,
+    CardsComponent,
+    FeedbackComponent,
+    ProfiloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/profilo/profilo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profilo',
+  templateUrl: './profilo.component.html',
+  styleUrls: ['./profilo.component.sass']
+})
+export class ProfiloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
