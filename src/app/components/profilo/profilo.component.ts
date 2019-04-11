0eca9a7d01cc3e6f8108bfc37cc8683cfdd85f24
lscutillo <lscutillo@gmail.com>
2019-04-11 12:53:02 +0200

    sistemato profilo

--- src/app/components/profilo/profilo.component.ts
+++ src/app/components/profilo/profilo.component.ts
@@ -3,9 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profilo',
   templateUrl: './profilo.component.html',
-  styleUrls: ['./profilo.component.sass']
+  styleUrls: ['./profilo.component.scss']
 })
 export class ProfiloComponent implements OnInit {
+  name : string = localStorage.name;
+  surname :string = localStorage.surname;
+  sex : string = localStorage.sex;
+  email : string = localStorage.email;
+  phone : string = localStorage.phone;
+  
+  
 
   constructor() { }
 
